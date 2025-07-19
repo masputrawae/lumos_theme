20f05aaeb7081ed0b8ddffca3e847c8d96e242eb
Masputrawae <masputrawae.official@gmail.com>
2025-07-19 11:21:14 +0700

    Changed On 📅 : 2025-07-19 11:21:14

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 import { themeHandler } from './module_internal/switch_theme'
 import { offcanvasHandler } from './module_internal/offcanvas'
 import { treeHandler } from './module_internal/tree_collapsible'
+import { tocHandler } from './module_internal/toc_highlight'
 
 function main() {
   themeHandler()
   offcanvasHandler()
   treeHandler()
+  tocHandler()
 }
 
 document.addEventListener('DOMContentLoaded', main)
