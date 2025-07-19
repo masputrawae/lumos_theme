5afafd47414efb9e30b723e5ed7083a22b337cc0
Masputrawae <masputrawae.official@gmail.com>
2025-07-19 12:01:30 +0700

    Changed On 📅 : 2025-07-19 12:01:30

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,14 @@ import { offcanvasHandler } from './module_internal/offcanvas'
 import { treeHandler } from './module_internal/tree_collapsible'
 import { tocHandler } from './module_internal/toc_highlight'
 import { modalSearchHandler } from './module_internal/search'
-
+import { searchHandler } from './module_internal/search'
 function main() {
   themeHandler()
   offcanvasHandler()
   treeHandler()
   tocHandler()
   modalSearchHandler()
+  searchHandler();
 }
 
 document.addEventListener('DOMContentLoaded', main)
