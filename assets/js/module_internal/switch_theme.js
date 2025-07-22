994bf5eb0a3d5e187dac8e2e92565057537a09f7
Masputrawae <masputrawae.official@gmail.com>
2025-07-22 11:48:37 +0700

    Changed On 📅 : 2025-07-22 11:48:37

--- assets/js/module_internal/switch_theme.js
+++ assets/js/module_internal/switch_theme.js
@@ -7,6 +7,31 @@ export function themeHandler() {
     elHtml.dataset.theme = theme
     buttons.forEach(btn => btn.classList.toggle('isDark', theme === 'dark'))
     sessionStorage.setItem(KEY_THEME, theme)
+    updateGiscusTheme(theme)
+  }
+
+  const updateGiscusTheme = theme => {
+    const apply = () => {
+      const iframe = document.querySelector('iframe.giscus-frame')
+      if (!iframe) return false
+
+      iframe.contentWindow.postMessage(
+        {
+          giscus: {
+            setConfig: {
+              theme: theme === 'dark' ? 'dark' : 'light',
+            },
+          },
+        },
+        'https://giscus.app'
+      )
+
+      return true
+    }
+    if (apply()) return
+    const interval = setInterval(() => {
+      if (apply()) clearInterval(interval)
+    }, 1000)
   }
 
   const initTheme = () => {
